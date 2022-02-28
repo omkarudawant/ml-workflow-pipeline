c74243cb3780152ea04251fa7514030e72920d87
omkarudawant <omkarudawant97@gmail.com>
2022-02-28 15:09:43 +0530

    added flask code for running bash

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import os
-
+import subprocess
+from subprocess import Popen, PIPE
+from subprocess import check_output
 from flask import Flask
 
 app = Flask(__name__)
 
 
+def get_shell_script_output_using_communicate():
+    session = Popen(['./run_kedro.sh'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = session.communicate()
+    if stderr:
+        raise Exception("Error " + str(stderr))
+    return stdout.decode('utf-8')
+
+
+def get_shell_script_output_using_check_output():
+    stdout = check_output(['./run_kedro.sh']).decode('utf-8')
+    return stdout
+
+
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+    return '<pre>' + get_shell_script_output_using_check_output() + '</pre>'
 
 
 if __name__ == "__main__":
